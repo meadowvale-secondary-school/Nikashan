c66580e10375e7cd985b3262b44b8f28694abea6
Nikashan Thavaruban <nikashan02@gmail.com>
2019-03-20 13:57:17 -0400

    no message

--- Xcode/Student Resources/1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/9. Exercise - Type Inference and Required Values.xcplaygroundpage/Contents.swift
+++ Xcode/Student Resources/1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/9. Exercise - Type Inference and Required Values.xcplaygroundpage/Contents.swift
@@ -4,7 +4,7 @@
  Declare a variable called `name` of type `String`, but do not give it a value. Print `name` to the console. Does the code compile? Remove any code that will not compile.
  */
 var name:String
-
+print("Hello")
 /*:
  Now assign a value to `name`, and print it to the console.
  */
